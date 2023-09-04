d42908d5e6e36989a21e3e24a95d4f56fdf73a77
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-09-04 17:52:53 +0000

    Adding of index to make the code simple to read

---
+++ src/components/index.tsx
@@ -0,0 +1,13 @@
+import AboutUsComponent from "./AboutUsComponent";
+import HeroComponent from "./Hero.component";
+import OurServicesComponent from "./OurServicesComponent";
+
+
+
+// Exporting all components
+
+export {
+    AboutUsComponent,
+    HeroComponent,
+    OurServicesComponent
+}
