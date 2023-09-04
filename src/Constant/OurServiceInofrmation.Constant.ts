485b60dbcd74b68bb7b5848e666bfa79d6a446a0
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-09-04 17:51:47 +0000

    Adding of new constand  folder to make everything nice and readable

---
+++ src/Constant/OurServiceInofrmation.Constant.ts
@@ -0,0 +1,40 @@
+
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+
+} from "@heroicons/react/20/solid";
+
+export  const features = [
+  {
+    name: "Range of Service.",
+    description:
+      "flexible business processes that respond to customer demands and the range of requests.Examples include: Service based on customer segmentation  Providing the perception of dedicated support 24 hours a day, 7 days a week, customer service availability.",
+    icon: CloudArrowUpIcon,
+    href: "#",
+  },
+  {
+    name: "Ease of Service",
+    description:
+      " allow customers to choose the media option for doing business and creating cross channel customer service. Examples include:Providing integrated, alternate access points (channels) for customer service (phone, fax, email, postal mail and Internet) Allowing anytime customer service Consistent information and service across. ",
+    icon: LockClosedIcon,
+    href: "#",
+  },
+  {
+    name: "Quality & Accuracy of Service",
+    description:
+      "personalized service based on the customer profile and  customer segmentation rules. Examples include:Knowledgeable personnel performing value-added activities in support of the customer Accurate transaction processing and validation via knowledge-based infrastructure (whether performed by company personnel or directly by the customer)",
+    icon: ArrowPathIcon,
+    href: "#",
+  },
+  {
+    name: "Speed of Service",
+    description:
+      "Ensuring that when a customer contacts the organization, the inquiry or problem is handled timely, efficient and during the first contact. Examples include:  Responsiveness to all contact methods utilizing an efficient workflow with minimal hand-offs  Providing an intuitive customer self-help interface with immediate access to information and people.",
+    icon: FingerPrintIcon,
+    href: "#",
+  },
+
+];
